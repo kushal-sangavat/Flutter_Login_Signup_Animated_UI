e3d8c83f7caae6a63460d3ff93572a042b50b681
kushal-sangavat <kushalsangavat.46@gmail.com>
2025-12-02 21:29:21 +0530

    app icon updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,4 +4,4 @@ const Color login_bg = Color(0xFF00C470);
 const Color signup_bg = Color(0xFF000A54);
 
 const double defpaultPadding = 16.0;
-const Duration defaultDuration = Duration(milliseconds: 300);
+const Duration defaultDuration = Duration(milliseconds: 200);
